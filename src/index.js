cc8e8d218efb2c84be81c816a87a1d073dc0fc66
Alex Middeleer <almiddeleer@gmail.com>
2022-02-23 23:29:52 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ class Result {
         this.total = 0;
         this.passed = 0;
     }
+    get allPassed() {
+        return this.total && this.total === this.passed;
+    }
 }
-module.exports = function({tests} = {}) {
+
+const anArray = x => Array.isArray(x) ? x : [];
+const aFunction = x => typeof x === 'function' ? x : () => 0;
+
+module.exports = function({tests, testRunner} = {}) {
     const result = new Result();
-    result.total = tests && tests.length || 0;
+    tests = anArray(tests);
+    testRunner = aFunction(testRunner);
+    result.total = tests.length;
+    result.passed = tests.reduce((total, t) => {
+        return total + testRunner(t) ? 1 : 0;
+    }, 0);
     return result;
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,31 @@
 const testFn = require('../src/index');
 
+const allTestsPassTestRunner = () => true;
+const minimalTest = {};
+
+const expectResultToHave = (result, {total, passed, allPassed}) => {
+    expect(typeof result).toBe('object');
+    expect(result.total).toBe(total);
+    expect(result.passed).toBe(passed);
+    expect(!!result.allPassed).toBe(!!allPassed);
+};
+
 test('calling test returns a report', () => {
     const result = testFn();
-    expect(typeof result).toBe('object');
-    expect(result.total).toBe(0);
-    expect(result.passed).toBe(0);
-    expect(result.allPassed).toBeFalsy();
+    expectResultToHave(result, { total: 0, passed: 0, allPassed: false });
 });
 
 test('calling test with a single test', () => {
     const result = testFn({
-        tests: [{}]
+        tests: [minimalTest]
     });
-    expect(typeof result).toBe('object');
-    expect(result.total).toBe(1);
-    expect(result.passed).toBe(0);
-    expect(result.allPassed).toBeFalsy();
+    expectResultToHave(result, { total: 1, passed: 0, allPassed: false });
+});
+
+test('calling test with a single passing test', () => {
+    const result = testFn({
+        tests: [minimalTest],
+        testRunner: allTestsPassTestRunner
+    });
+    expectResultToHave(result, { total: 1, passed: 1, allPassed: true });
 });
