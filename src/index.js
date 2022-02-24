02c93d986301f9e8663ed03adbbc9205fbad9fa3
Alex Middeleer <almiddeleer@gmail.com>
2022-02-23 23:03:55 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+class Result {
+    constructor() {
+        this.total = 0;
+        this.passed = 0;
+    }
+}
 module.exports = function({tests} = {}) {
-    return {
-        total: tests && tests.length || 0,
-        passed: 0
-    };
+    const result = new Result();
+    result.total = tests && tests.length || 0;
+    return result;
 };
