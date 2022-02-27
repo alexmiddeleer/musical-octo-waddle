2a6625598e7ef3303b17e739f4e392481aaff957
Alex Middeleer <almiddeleer@gmail.com>
2022-02-26 23:02:16 -0500

    Record which tests failed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ class Result {
     constructor() {
         this.total = 0;
         this.passed = 0;
+        this.failedTests = [];
     }
     get allPassed() {
         return this.total && this.total === this.passed;
+    }
+    recordFailure(test) {
+        this.failedTests.push(test);
     }
 }
 
@@ -16,8 +20,11 @@ module.exports = function({tests, testRunner} = {}) {
     tests = anArray(tests);
     testRunner = aFunction(testRunner);
     result.total = tests.length;
-    result.passed = tests.reduce((total, t) => {
-        return total + testRunner(t) ? 1 : 0;
-    }, 0);
+    tests.forEach(t => {
+        if(!testRunner(t)) {
+            result.recordFailure(t);
+        }
+    });
+    result.passed = tests.length - result.failedTests.length;
     return result;
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -8,6 +8,8 @@ const expectResultToHave = (result, {total, passed, allPassed}) => {
     expect(result.total).toBe(total);
     expect(result.passed).toBe(passed);
     expect(!!result.allPassed).toBe(!!allPassed);
+    expect(Array.isArray(result.failedTests)).toBeTruthy();
+    expect(result.failedTests.length).toBe(total-passed);
 };
 
 test('calling test returns a report', () => {
@@ -20,6 +22,7 @@ test('calling test with a single test', () => {
         tests: [minimalTest]
     });
     expectResultToHave(result, { total: 1, passed: 0, allPassed: false });
+    expect(result.failedTests[0]).toBe(minimalTest);
 });
 
 test('calling test with a single passing test', () => {
